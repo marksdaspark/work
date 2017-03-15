a2d87c95e3010d69bc88911b7aa930df6888ab51
mark <marksdaspark@gmail.com>
2017-03-15 19:32:51 +0800

    update week 2 homework

---
+++ notebooks/02_mcdonalds_homework.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 15 17:38:12 2017
+
+@author: mark
+"""
+
+# Imports
+
+Import pandas as pd
+
+
+path = r'C:\Users\mark\Anaconda3\envs\work\data\mcdonalds.csv'
